40e622de79ad3c916a150405448df2e66f3b9a1d
Abrham-m <abrhammen14@gmail.com>
2024-08-01 17:31:05 +0300

    javascript file is updated

--- script.js
+++ script.js
@@ -9,9 +9,26 @@ const calculateAge = () => {
   let monthDifference = presentDate.getMonth() - birthDate.getMonth();
   // calculating the day difference
   let dayDifference = presentDate.getDate() - birthDate.getDate();
-  // check if the input date is invalid
-  if (yearDifference < 0) {
-    console.log("Invalid Date");
+  // check if the input date is invalid or not selected
+  if (yearDifference < 0 || isNaN(birthDate.getTime())) {
+    document.querySelector(
+      ".ageResult"
+    ).innerHTML = `<span style="color: red;">⚠ Invalid Date. Please enter a valid date.</span>`;
+    // If the day difference is negative, the month difference is decremented by 1.
+    if (dayDifference < 0) {
+      monthDifference -= 1;
+    }
+    // If the month difference is negative, the year difference is decremented by 1.
+    if (monthDifference < 0) {
+      yearDifference -= 1;
+    }
+    // This ensures the age calculation is accurate when the current date is before the birth date.
+    document.querySelector(
+      ".ageResult"
+    ).textContent = `Your are ${yearDifference} years old.`;
+    document.querySelector(".ageResult").style.fontSize = "18px";
+    document.querySelector(".ageResult").style.fontWeight = "bold";
+    document.querySelector(".ageResult").style.color = "#2f75e5";
   } else {
     if (dayDifference < 0) {
       monthDifference -= 1;
@@ -19,9 +36,11 @@ const calculateAge = () => {
     if (monthDifference < 0) {
       yearDifference -= 1;
     }
-    // console.log(yearDifference, monthDifference, dayDifference);
-    document.querySelector(".ageResult").textContent = yearDifference;
-
-    console.log(`Your age is ${yearDifference} years`);
+    document.querySelector(
+      ".ageResult"
+    ).textContent = `Your are ${yearDifference} years old.`;
+    document.querySelector(".ageResult").style.fontSize = "18px";
+    document.querySelector(".ageResult").style.fontWeight = "bold";
+    document.querySelector(".ageResult").style.color = "#2f75e5";
   }
 };
